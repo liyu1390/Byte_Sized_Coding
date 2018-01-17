42f9745a820e9a9e6cf0e1e8d371fb39398f236a
Li <liyu@berkeley.edu>
2018-01-17 07:39:00 -0800

    Renamed variables. TODO: Complete terminating if statement.

--- Median_Of_Two_Sorted_Arrays/main.cpp
+++ Median_Of_Two_Sorted_Arrays/main.cpp
@@ -1,23 +1,34 @@
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int cutOne = 0, cutTwo = 0;
-    int lower = 0, upper = nums1.size()-1;
-    int halfSize = (nums1.size() + nums2.size())/2;
+    switchSmallerToFirst(nums1, nums2);
+    int m = nums1.size(), n = nums2.size();
+    int imin = 0, imax = m;
+    int halfSize = (nums1.size() + nums2.size() + 1)/2;
     while (true)
     {
-        cutOne = (upper+lower)/2;
-        cutTwo = halfSize - cutOne;
-        if (nums1[cutOne-1] > nums2[cutTwo])
+        i = (imin + imax)/2;
+        j = halfSize - i;
+        if ((i == 0 or j == n or (nums1[i-1] <= nums2[j]))
+             and (j == 0 or i == m or (nums2[j-1] <= num1[i])))
         {
-            upper = cutOne;
+            
+        }
+        else if (nums1[cutOne-1] > nums2[cutTwo])
+        {
+            imax = i - 1;
         }
         else if (nums1[cutOne] < nums2[cutTwo-1])
         {
-            lower = cutOne;
-        }
-        else
-        {
-            break;
+            imin = i + 1;
         }
     }
-    return nums1[cutOne];
+}
+
+void switchSmallerToFirst(vector<int>* nums1, vector<int>* nums2)
+{
+    if (nums1->size() > nums2->size())
+    {
+        vector<int>* tmp = nums1;
+        nums1 = nums2;
+        nums2 = tmp;
+    }
 }
