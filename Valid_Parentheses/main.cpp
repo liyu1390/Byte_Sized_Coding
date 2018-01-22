fc0985365b86c8cfc4347862ad9a5091eb32def2
Li <liyu@berkeley.edu>
2018-01-21 19:31:23 -0800

    Added main function.

--- Valid_Parentheses/main.cpp
+++ Valid_Parentheses/main.cpp
@@ -1,3 +1,20 @@
 bool isValid(string s) {
-        
+    list<char> stack;
+    map<char, char> pairings;
+    pairings[')'] = '(';
+    pairings['}'] = '{';
+    pairings[']'] = '[';
+    
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (pairings.count(s[i]) == 0)
+            stack.push_back(s[i]);
+        else if (!stack.empty() && stack.back() == pairings[s[i]])
+            stack.pop_back();
+        else
+            return false;
+    }
+    if (stack.empty())
+        return true;
+    return false;
 }
