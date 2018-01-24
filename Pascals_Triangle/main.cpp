26d06936ff8b257e935c03322697764219b7b69c
Li <liyu@berkeley.edu>
2018-01-23 23:18:49 -0800

    Completed first Pascal's Triangle.

---
+++ Pascals_Triangle/main.cpp
@@ -0,0 +1,25 @@
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> vec;
+    
+    if (numRows <= 0)
+        return vec;
+    
+    vec.push_back({1});
+    for (int i = 0; i < numRows-1; ++i)
+    {
+        vec.push_back(createNextLevel(vec[i]));
+    }
+    
+    return vec;
+}
+
+vector<int> createNextLevel(vector<int>& vec){
+    int prevWidth = vec.size();
+    vector<int> newVec(prevWidth + 1);
+    
+    for (int i = 1; i < prevWidth; ++i)
+    {
+        newVec[i] = vec[i-1] + vec[i];
+    }
+    newVec[0] = 1, newVec[prevWidth] = 1;
+}
