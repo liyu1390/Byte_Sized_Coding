a7ca80629267c697a60b392007a0085d90513c8f
Li <liyu@berkeley.edu>
2018-01-31 07:26:06 -0800

    Cleaned up code.

--- Binary_Tree_Paths/main.cpp
+++ Binary_Tree_Paths/main.cpp
@@ -1,24 +1,13 @@
 vector<string> binaryTreePaths(TreeNode* root) {
-    vector<string> newVec;
-    if (root == NULL) return newVec;
-    string strVal = to_string(root->val);
-    if (root->left == NULL && root->right == NULL)
-        return {strVal};
-    if (root->left != NULL)
-    {
-        auto leftPaths = binaryTreePaths(root->left);
-        for (auto it = leftPaths.begin(); it != leftPaths.end(); ++it)
-        {
-            newVec.push_back(strVal + "->" + *it);
-        }
-    }
-    if (root->right != NULL)
-    {
-        auto rightPaths = binaryTreePaths(root->right);
-        for (auto it = rightPaths.begin(); it != rightPaths.end(); ++it)
-        {
-            newVec.push_back(strVal + "->" + *it);
-        }
-    }
-    return newVec;
+    vector<string> result;
+    if (root == NULL) return result;
+    binaryTreePaths(result, root, to_string(root->val));
+    return result;
+}
+
+void binaryTreePaths(vector<string>& paths, TreeNode* root, string curString)
+{
+    if (root->left) binaryTreePaths(paths, root->left, curString + "->" + to_string(root->left->val));
+    if (root->right) binaryTreePaths(paths, root->right, curString + "->" + to_string(root->right->val));
+    if (!root->left && !root->right) paths.push_back(curString);
 }
