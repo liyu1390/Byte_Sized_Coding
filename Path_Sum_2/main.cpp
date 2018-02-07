f02a3a2d5eadba1f24cdb720d22d0040d9e6d7d8
Li <liyu@berkeley.edu>
2018-02-06 23:59:55 -0800

    Cleaned up code.

--- Path_Sum_2/main.cpp
+++ Path_Sum_2/main.cpp
@@ -1,22 +1,18 @@
 vector<vector<int>> pathSum(TreeNode* root, int sum) {
     vector<vector<int>> paths;
     vector<int> path;
-    if (root == NULL)
-        return paths;
     findPaths(root, sum, path, paths);
     return paths;
 }
 
 void findPaths(TreeNode* root, int remainder, vector<int>& path, vector<vector<int>>& paths){
+    if (root == NULL)
+        return;
     path.push_back(root->val);
-    if (root->left != NULL) {
-        findPaths(root->left, remainder - root->val, path, paths);
-    }
-    if (root->right != NULL) {
-        findPaths(root->right, remainder - root->val, path, paths);
-    }
     if (root->left == NULL && root->right == NULL && remainder == root->val) {
         paths.push_back(path);
     }
+    findPaths(root->left, remainder - root->val, path, paths);
+    findPaths(root->right, remainder - root->val, path, paths);
     path.pop_back();
 }
