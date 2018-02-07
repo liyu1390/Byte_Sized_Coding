68d1802c8c35fd4ad534be24bd3d14dca2472542
Li <liyu@berkeley.edu>
2018-02-07 07:28:22 -0800

    Added readme and completed main function.

---
+++ Path_Sum_3/main.cpp
@@ -0,0 +1,10 @@
+int pathSum(TreeNode* root, int sum) {
+    if (!root) return 0;
+    return sumUp(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+}
+
+int sumUp(TreeNode* node, int pre, int sum) {
+    if (!node) return 0;
+    int cur = pre + node->val;
+    return (cur == sum) + sumUp(node->left, cur, sum) + sumUp(node->right, cur, sum);
+}
