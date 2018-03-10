8cee41db0b767b6eb69fe2a9f98c2421f4db78d0
Li <liyu@berkeley.edu>
2018-03-10 15:36:58 -0800

    Completed Min_Stack.

--- Min_Stack/main.cpp
+++ Min_Stack/main.cpp
@@ -1,23 +1,25 @@
 class MinStack {
+private:
+    vector<pair<int,int>> stack;
 public:
     /** initialize your data structure here. */
     MinStack() {
-        
     }
     
-    void push(int x) {
-        
+    void push(int x){
+        int minVal = stack.empty() ? x : min(getMin(), x);
+        stack.push_back(make_pair(x, minVal));
     }
     
     void pop() {
-        
+        stack.pop_back();
     }
     
     int top() {
-        
+        return stack.back().first; 
     }
     
     int getMin() {
-        
+        return stack.back().second;
     }
 };
