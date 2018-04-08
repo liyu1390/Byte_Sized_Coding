d20ee5a632a776a56f5fa9a677ace31d0fe28847
Li <liyu@berkeley.edu>
2018-04-07 23:51:23 -0700

    Completed Majority_Element.

--- Majority_Element/main.cpp
+++ Majority_Element/main.cpp
@@ -1,3 +1,11 @@
 int majorityElement(vector<int>& nums) {
-        
+    int curCandidate;
+    int count = 0;
+    for (auto num : nums)
+    {
+    	if (count == 0)
+    		curCandidate = num;
+    	count += (num == curCandidate) ? 1 : -1;
+    }
+    return curCandidate;
 }
