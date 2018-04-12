e18e8b72920ef11e2100e9308a68be199b332951
Li <liyu@berkeley.edu>
2018-04-11 21:02:28 -0700

    Contains_Duplicate: Completed.

--- Contains_Duplicate/main.cpp
+++ Contains_Duplicate/main.cpp
@@ -1,3 +1,10 @@
 bool containsDuplicate(vector<int>& nums) {
-    
+    unordered_set<int> uniqueNums;
+    for (auto num : nums)
+    {
+        if (uniqueNums.find(num) != uniqueNums.end())
+            return true;
+        uniqueNums.insert(num);
+    }
+    return false;
 }
