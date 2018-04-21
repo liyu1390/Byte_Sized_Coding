9f320deb1e0dcaa3dbf6669ba773b762119fec67
Li <liyu@berkeley.edu>
2018-04-21 12:36:58 -0700

    Containts_Duplicate_II: Completed

--- Contains_Duplicate_II/main.cpp
+++ Contains_Duplicate_II/main.cpp
@@ -1,3 +1,15 @@
 bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
+    map<int, int> seen;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(seen.find(nums[i]) != seen.end() && abs(i - seen.find(nums[i])->second) <= k)
+        {
+            return true;
+        }
+        else
+        {
+            seen[nums[i]] = i;
+        }
+    }
+    return false;
 }
