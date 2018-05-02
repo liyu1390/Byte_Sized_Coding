3c81ab0c3d92da3c630d8f772b8672a0816a14af
Li <liyu@berkeley.edu>
2018-05-02 07:28:30 -0700

    Happy_Number: refactored.

--- Happy_Number/main.cpp
+++ Happy_Number/main.cpp
@@ -1,18 +1,23 @@
-bool isHappy(int n) {
+bool isHappy(int n) 
+{
     set<int> seen;
     while(n != 1)
     {
         if (seen.find(n) != seen.end())
             return false;
-        else
-            seen.insert(n);
-        int sum = 0;
-        while (n != 0)
-        {
-            sum += (pow(n%10,2));
-            n /= 10;
-        }
-        n = sum;
+        seen.insert(n);
+        n = sumOfSquaredDigits(n);
     }
     return true;
+}
+
+int sumOfSquaredDigits(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum += pow(n%10,2);
+        n /= 10;
+    }
+    return sum;
 }
