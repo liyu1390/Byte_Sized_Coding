28b03591ac9f7780138ff8edf3973d1ad679a30c
Li <liyu@berkeley.edu>
2018-05-12 19:25:51 -0700

    Combination_Sum: completed.

--- Combination_Sum/main.cpp
+++ Combination_Sum/main.cpp
@@ -1,4 +1,34 @@
-vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> ret;
+    vector<int> curComb;
+    sort(candidates.begin(), candidates.end());
+    findCombSums(ret, curComb, candidates, 0, target);
+    return ret;
+}
+
+//assume candidates has unique elements and sorted in order.
+//returns true if we have found a candidate.
+void findCombSums(vector<vector<int>>& ret, vector<int>& curComb, vector<int>& candidates, int checkInd, int remainder)
 {
-        
+    if (remainder == 0)
+    {
+        ret.push_back(vector<int>(curComb));
+        return;
+    }
+    if (checkInd >= candidates.size() || candidates[checkInd] > remainder) 
+        return;
+    
+    int candidate = candidates[checkInd];
+    int maxRep = remainder/candidate;
+    int i;
+    for(i = 0; i < maxRep; i++)
+    {
+        curComb.push_back(candidate);
+    }
+    for(; i > 0; i--)
+    {
+        findCombSums(ret, curComb, candidates, checkInd+1, remainder - candidate*i);
+        curComb.pop_back();
+    }
+    findCombSums(ret, curComb, candidates, checkInd+1, remainder);   
 }
