84a9cd3968e45eaee0f93c83170f842b4a02fdbf
Li <liyu@berkeley.edu>
2018-05-13 00:31:47 -0700

    Combination_Sum_III: completed.

--- Combination_Sum_III/main.cpp
+++ Combination_Sum_III/main.cpp
@@ -1,3 +1,26 @@
 vector<vector<int>> combinationSum3(int k, int n) {
-        
+    vector<vector<int>> ret;
+    vector<int> currentComb;
+    
+    getCombinations(ret, currentComb, k, 1, n);
+    
+    return ret;
+}
+
+void getCombinations(vector<vector<int>>& ret, vector<int>& currentComb, int k, int start, int remainder)
+{
+    if (remainder < 0 || currentComb.size() > k)
+        return;
+    if (remainder == 0 && currentComb.size() == k)
+    {
+        ret.push_back(vector<int>(currentComb));
+        return;
+    }
+    
+    for(int i = start; i <= 9; i++)
+    {
+        currentComb.push_back(i);
+        getCombinations(ret, currentComb, k, i+1, remainder-i);
+        currentComb.pop_back();
+    }
 }
