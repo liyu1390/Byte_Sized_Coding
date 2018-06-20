f1d58dffdc0fcae1924071f0c93d4ffe3e799025
Li <liyu@berkeley.edu>
2018-06-19 21:13:19 -0700

    Combination_Sum_IV: completed.

--- Combination_Sum_IV/main.cpp
+++ Combination_Sum_IV/main.cpp
@@ -1,3 +1,15 @@
 int combinationSum4(vector<int>& nums, int target) {
-        
+    vector<int> combs(target + 1, 0);
+    combs[0] = 1;
+    for(int i = 0; i <= target; i++)
+    {
+        for(int j = 0; j < nums.size(); j++)
+        {
+            if(i - nums[j] >= 0)
+            {
+                combs[i] += combs[i - nums[j]];
+            }
+        }
+    }
+    return combs[target];
 }
